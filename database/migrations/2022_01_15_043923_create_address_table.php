a9425b56bee0e25bd08acaff967f5288bb0fbe6c
Neshanth <m.neshanth13@gmail.com>
2022-01-15 10:17:31 +0530

    added address tablw

---
+++ database/migrations/2022_01_15_043923_create_address_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('address', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname');
+            $table->string('lastname')->nullable();
+            $table->longText('full_address');
+            $table->string('state');
+            $table->string('city');
+            $table->unsignedBigInteger('postcode');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('address');
+    }
+}
