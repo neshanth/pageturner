a186ea31c0f2bedf4eb4a4be071a7a5a5aa1995c
Neshanth <m.neshanth13@gmail.com>
2022-01-28 08:29:38 +0530

    added guest cart functionality

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -4,56 +4,75 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use App\Models\UserRole;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use App\Models\Cart;
 
 class AuthController extends Controller
 {
-   public function login()
-   {
-       return view("auth.login");
-   }
-   public function register()
-   {
-       return view("auth.register");
-   }
-   public function registerPost(Request $request)
-   {
-       $request->validate([
-          'name' => "required",
-          'email' => "required|email|unique:users",
-          'password' => "required|min:5"
-      ]);
-       $user = User::create([
-          'name' => $request->name,
-          'email' => $request->email,
-          'password' => Hash::make($request->password)
-      ]);
-       $user->assignRole("User");
-      return redirect()->back()->with('success','Registration Successful');
-   }
-   public function loginPost(Request $request)
-   {
+    public function login()
+    {
+        return view("auth.login");
+    }
+    public function register()
+    {
+        return view("auth.register");
+    }
+    public function registerPost(Request $request)
+    {
         $request->validate([
-           'email' => 'required',
-           'password' => 'required'
-       ]);
-        $credentials = $request->only('email','password');
-       if(Auth::attempt($credentials))
-       {
-           $request->session()->regenerate();
-           return redirect("/");
-       }
-       return redirect()->back()->with("error", "Login Failed");
-   }
-   public function logout(Request $request)
-   {
-       Auth::logout();
-       $request->session()->invalidate();
-       $request->session()->regenerate();
-       return redirect("/");
-   }
+            'name' => "required",
+            'email' => "required|email|unique:users",
+            'password' => "required|min:5"
+        ]);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+        $user->assignRole("User");
+        return redirect()->back()->with('success', 'Registration Successful');
+    }
+    public function loginPost(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+
+            if ($request->session()->get("cart") !== null) {
+                $customerId = Auth::user()->id;
+                $this->getGuestCart($request, $customerId);
+                $request->session()->forget("cart");
+                return redirect("/cart/show");
+            }
+
+            $request->session()->regenerate();
+            return redirect("/");
+        }
+        return redirect()->back()->with("error", "Login Failed");
+    }
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerate();
+        return redirect("/");
+    }
+
+    private function getGuestCart(Request $request, $customerId)
+    {
+        $cartItems =  $request->session()->get("cart");
+        foreach ($cartItems as $cartItem) {
+            $cart = new Cart;
+            $cart->product_id  = $cartItem['product_id'];
+            $cart->customer_id = $customerId;
+            $cart->quantity = $cartItem['quantity'];
+            $cart->price = $cartItem['price'];
+            $cart->save();
+        }
+    }
 }
--- app/Http/Controllers/Homepage/HomepageController.php
+++ app/Http/Controllers/Homepage/HomepageController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Homepage;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Models\Product;
 
